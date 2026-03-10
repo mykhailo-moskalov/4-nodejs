974dc3981e8afa8b569edff9a080787882a4ec1d
mishokkartoshki <misha@moskalov.com>
2026-03-10 18:10:12 +0100

    8

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -47,3 +47,45 @@ export const loginUser = async (req, res) => {
 
   res.status(200).json(user);
 };
+
+export const logoutUser = async (req, res) => {
+  const { sessionId } = req.cookies;
+
+  if (sessionId) {
+    await Session.deleteOne({ _id: sessionId });
+  }
+
+  res.clearCookie('sessionId');
+  res.clearCookie('accessToken');
+  res.clearCookie('refreshToken');
+
+  res.status(204).send();
+};
+
+export const refreshUserSession = async (req, res) => {
+  const session = await Session.findOne({
+    _id: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  const isSessionTokenExpired =
+    new Date() > new Date(session.refreshTokenValidUntil);
+
+  if (isSessionTokenExpired) {
+    throw createHttpError(401, 'Session token expired');
+  }
+
+  await Session.deleteOne({
+    _id: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  const newSession = await createSession(session.userId);
+  setSessionCookies(res, newSession);
+
+  res.status(200).json({ message: 'Session refreshed' });
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,11 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { loginUser, registerUser } from '../controllers/authController.js';
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshUserSession,
+} from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
@@ -10,5 +15,7 @@ const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshUserSession);
 
 export default router;
